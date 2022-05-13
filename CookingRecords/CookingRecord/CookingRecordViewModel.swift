fe3fb185279c9cc067356aece1de33ea5969abe4
taisei <ninzin60en.dev@gmail.com>
2022-05-14 03:28:00 +0900

    [add]レコード詳細画面の作成
    レコード詳細画面の新規作成。

---
+++ CookingRecords/CookingRecord/CookingRecordViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  CookingRecordViewModel.swift
+//  CookingRecords
+//
+//  Created by 住山大誠 on 2022/05/14.
+//
+
+import Foundation
+class CookingRecordViewModel {
+    var record = CookingRecord(imageUrl: "https://cooking-records.herokuapp.com/images/1.jpg", comment: "パンチのきいた辛味が印象的です。", recipeType: CookingRecord.RecipeType.mainDish, recordedAt: "2018-04-21 14:04:22")
+}
