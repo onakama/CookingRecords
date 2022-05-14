b690572dae3f301b1d7849e18ae96ad19001aa16
taisei <ninzin60en.dev@gmail.com>
2022-05-15 01:26:57 +0900

    [add]apiRequest
    非同期APIリクエストの実装

--- CookingRecords/CookingRecordList/CookingRecordListModel.swift
+++ CookingRecords/CookingRecordList/CookingRecordListModel.swift
@@ -7,11 +7,24 @@
 
 import Foundation
 class CookingRecordListModel {
-    func jsonDecode() throws -> ResponseData {
+    
+    func httpGet() async throws -> ResponseData {
+        let url = URL(string: "https://cooking-records.herokuapp.com/cooking_records?offset=1&limit=100")!
+        let request = URLRequest(url: url)
+        let (data, _) = try await URLSession.shared.data(for: request)
+        do {
+            let data = try jsonDecode(json: data)
+            return data
+        } catch let error {
+            throw error
+        }
+    }
+
+    func jsonDecode(json: Data) throws -> ResponseData {
         let decoder = JSONDecoder()
         decoder.keyDecodingStrategy = .convertFromSnakeCase
         decoder.dateDecodingStrategy = .formatted(.formatter)
-        guard let decodedData: ResponseData = try? decoder.decode(ResponseData.self, from: MOCK!) else {
+        guard let decodedData: ResponseData = try? decoder.decode(ResponseData.self, from: json) else {
             throw CookingRecordError.DecodeError
         }
         return decodedData
